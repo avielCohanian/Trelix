4ebc625f23c182a9c9428789a2f93d037c54df1a
eli granat <granat1710@gmail.com>
2021-11-30 21:31:20 +0200

    add async service

--- src/cmp/menu/about-board.vue
+++ src/cmp/menu/about-board.vue
@@ -1,10 +1,16 @@
 <template>
-  <h1> about </h1>
+  <h1> Board admins </h1>
 </template>
 
 <script>
+import {boardService }from '../../service/board.service.js'
 export default {
+created(){
 
+    boardService.getById('b101').then(res=>{
+console.log(res);
+    })
+}
 }
 </script>
 
