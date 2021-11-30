8d5207f0c0e95d3e6aa6aa2d39c248e29daf4bf1
Aviel Cohanian <avial151515@gmail.com>
2021-11-30 20:00:39 +0200

    dinamic start

--- src/pages/card-edit.vue
+++ src/pages/card-edit.vue
@@ -1,12 +1,13 @@
 <template>
     <section class="card-edit">
         <h3>edit</h3>
-        <component :is="component" @trelix="trelix"> </component>
+        <component :is="component" @dynamicCmp="dynamicCmp"> </component>
     </section>
 </template>
 
 <script>
 import attachment from '../cmp/edit-attachment.vue';
+import trelix from '../cmp/edit-trelix.vue';
 export default {
     name: 'cardEdit',
     data() {
@@ -15,18 +16,18 @@ export default {
         };
     },
     methods: {
-        trelix() {
-            console.log('aayaellll');
+        dynamicCmp(x) {
+            this.component = `card-${x}`;
         },
+        // editRender() {
+        //     return 'card-attachment';
+        // },
     },
     components: {
         'card-attachment': attachment,
+        'card-trelix': trelix,
     },
-    computed: {
-        editRender() {
-            return 'card-attachment';
-        },
-    },
+    computed: {},
 };
 </script>
 
