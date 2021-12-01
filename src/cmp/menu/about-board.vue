8c956e20ba5d715587b32b7b8699526a355debd5
eli granat <granat1710@gmail.com>
2021-12-01 10:03:55 +0200

    update manu

--- src/cmp/menu/about-board.vue
+++ src/cmp/menu/about-board.vue
@@ -1,18 +1,38 @@
 <template >
-<section class="cmp-about">
+<section class="cmp-about" >
 
-  <h1 class="about"> Board admin </h1>
-  <h1>hi</h1>
+  <h2 class="about"> Board admin </h2>
+  <div class="about-name">
+  <avatar  :size=70 :username="`${board.createdBy.fullname}`" ></avatar>
+  {{board.createdBy.fullname}}
+  </div>
+  {{board.createdBy.mail}}
+  <!-- get user email -->
+<!-- <pre>
+  {{board}}
+</pre> -->
+    <h2 class="icon-description"> Description </h2>
 </section>
 </template>
 
 <script>
+import Avatar from 'vue-avatar'
 import {boardService }from '../../service/board.service.js'
 export default {
+
+   props:{
+    board:{type:Object}
+  },
+
+  
 created(){
+ 
 
     boardService.getById('b101').then(res=>{
     })
+}
+,components:{
+  Avatar
 }
 }
 </script>
--- src/cmp/menu/change-background.vue
+++ src/cmp/menu/change-background.vue
@@ -1,10 +1,45 @@
 <template>
-  <h1> background </h1>
+<section>
+  <div class="bcg-main">
+    <div >
+      Photos
+    </div>
+    <div>
+      Colors
+    </div>
+    <hr>
+    <h2> Custom </h2>
+    <div>
+      <h3 class="plus"></h3>
+    </div>
+  </div>
+  <div class="bcg-choice">
+    <ul>
+      <li v-for="(color,idx) in colors" :key="idx" :style="color">
+        bcgColor
+      </li>
+    </ul>
+  </div>
+</section>
 </template>
 
 <script>
 export default {
-
+  data(){
+    return{
+        colors:{
+            orange:{background:'rgb(210, 144, 52)'},
+            blue:{background:'rgb(0, 121, 191)'},
+            red:{background:'rgb(176, 70, 50)'},
+            green:{background:'rgb(81, 152, 57)'},
+            pink:{background:'rgb(205, 90, 145)'},
+            purple:{background:'rgb(137, 96, 158)'},
+            blueLight:{background:'rgb(0, 174, 204)'},
+            greenLight:{background:'rgb(75, 191, 107)'},
+            gray:{background:'rgb(131, 140, 145)'}
+        }
+}
+}
 }
 </script>
 
