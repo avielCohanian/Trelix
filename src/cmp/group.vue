5b9a420321e61ca807d94cf49ba7cb595fa7e3a2
Yael Oushky <yaeloushky@h-MacBook-Air-sl-Yael.local>
2021-12-04 12:58:55 +0200

    clear

--- src/cmp/group.vue
+++ src/cmp/group.vue
@@ -2,10 +2,6 @@
   <section class="group-container">
     <div class="group">
       <header>
-        <!-- <template v-if="!isEditTitle"> -->
-        <!-- <h3 @click="isEditTitle = !isEditTitle">
-                        {{ group.title }}
-                    </h3> -->
         <el-input
           class="edit-title"
           size="mini"
@@ -15,23 +11,12 @@
           @click.native="updateGroup"
           @blur="updateGroup"
         ></el-input>
-        <!-- </template> -->
-        <!-- <template v-if="isEditTitle"> -->
-        <!-- <el-input
-                    class="edit-title"
-                        size="mini"
-                        v-model="groupToEdit.title"
-                        @keyup.enter.native="updateGroup"
-                    ></el-input> -->
-        <!-- </template> -->
         <i class="el-icon-more" @click="openModal = !openModal"></i>
       </header>
       <div class="modal" v-if="openModal">
         <div class="title">
           <span class="material-icons-outlined" @click="openModal = !openModal"
-            >close</span
-          >
-
+            >close</span>
           <p>List actions</p>
         </div>
         <hr />
@@ -42,7 +27,6 @@
           <li @click="deleteGroup">Archive this list</li>
         </ul>
       </div>
-
       <div class="modal" v-if="isModalAdd">
         <div class="title">
           <span
@@ -50,7 +34,6 @@
             @click="isModalAdd = !isModalAdd"
             >close</span
           >
-
           <p>Options</p>
         </div>
         <hr />
@@ -59,36 +42,26 @@
           <li @click="dynamicCmp('labels')">Labels...</li>
           <li>Position...</li>
         </ul>
-         <div class="dynamic-cmp" v-if="component.currCmp">
-                    <header>
-                        <h2>{{ component.name }}</h2>
-                        <a @click="closeModel" class="el-icon-close"> </a>
-                    </header>
-                    <component
-                        :is="component.currCmp"
-                        :card="newCard"
-                        @dynamicCmp="dynamicCmp"
-                    >
-                    </component>
-                </div>
+        <div class="dynamic-cmp" v-if="component.currCmp">
+          <header>
+            <h2>{{ component.name }}</h2>
+            <a @click="closeModel" class="el-icon-close"> </a>
+          </header>
+          <component
+            :is="component.currCmp"
+            :card="newCard"
+            @dynamicCmp="dynamicCmp"
+          >
+          </component>
+        </div>
       </div>
-
-
-      <!-- <Container
-        @drop="onDrop"
-        :get-ghost-parent="getGhostParent"
-        :remove-on-drop-out="true"
-        @drop-ready="onDropReady"> -->
       <div class="card-container">
         <div class="card-scroll">
-          <Draggable v-for="card in cardsToShow" :key="card.id">
-            <div class="draggable-item">
+          <div v-for="card in cardsToShow" :key="card.id">
+            <div>
               <card @click="showEdit(card.id)" :card="card" />
             </div>
-          </Draggable>
-
-          <!-- </Container> -->
-
+          </div>
           <label v-if="isAddCard">
             <div class="btn-group">
               <el-input
@@ -99,7 +72,6 @@
                 @keyup.enter.native="addCard"
               >
               </el-input>
-
               <div class="btn-add">
                 <div class="left">
                   <el-button type="primary" @click="addCard">
@@ -128,7 +100,6 @@
         ></el-input>
       </label>
     </div>
-    
   </section>
 </template>
 
@@ -136,15 +107,15 @@
 import member from "./edit-member.vue";
 import label from "./edit-label.vue";
 import { boardService } from "../service/board.service";
-import { Container, Draggable } from "vue-smooth-dnd";
-import { applyDrag, generateItems } from "../service/util.service.js";
+// import { Container, Draggable } from "vue-smooth-dnd";
+// import { applyDrag, generateItems } from "../service/util.service.js";
 import card from "./card.vue";
 
 export default {
   components: {
     card,
-    Container,
-    Draggable,
+    // Container,
+    // Draggable,
     "card-members": member,
     "card-labels": label,
   },
@@ -157,28 +128,28 @@ export default {
       isEditTitle: false,
       isAddCard: false,
       openModal: false,
-       component: {
-                currCmp: null,
-                name: '',
-            },
+      component: {
+        currCmp: null,
+        name: "",
+      },
       groupToEdit: JSON.parse(JSON.stringify(this.group)),
       newCard: boardService.getEmptyCard(),
       cards: this.group.cards,
-      items: generateItems(10, (i) => ({
-        id: i,
-        data: "Draggable " + i,
-      })),
+      // items: generateItems(10, (i) => ({
+      //   id: i,
+      //   data: "Draggable " + i,
+      // })),
     };
   },
   methods: {
     dynamicCmp(cmp) {
-            console.log(cmp);
-            this.component.name = cmp;
-            this.component.currCmp = `card-${cmp}`;
-        },
-        closeModel() {
-            this.component.currCmp = null;
-        },
+      console.log(cmp);
+      this.component.name = cmp;
+      this.component.currCmp = `card-${cmp}`;
+    },
+    closeModel() {
+      this.component.currCmp = null;
+    },
     openModalAdd() {
       this.isModalAdd = !this.isModalAdd;
     },
@@ -235,23 +206,10 @@ export default {
         console.log(err);
       }
     },
-    onDrop(dropResult) {
-      // console.log(dropResult, 'dropResult');
-      this.cards = applyDrag(this.cards, dropResult);
-      console.log("drop ready", this.cards);
-    },
-    getGhostParent() {
-      return document.body;
-    },
-    onDropReady(dropResult) {
-      // this.cards.
-      // console.log('drop ready', dropResult);
-      // console.log('drop ready', this.cards);
-    },
   },
   computed: {
-    getEmptyCard(){
-return this.newCard
+    getEmptyCard() {
+      return this.newCard;
     },
     cardsToShow() {
       return this.group.cards;
--- src/cmp/groups.vue
+++ src/cmp/groups.vue
@@ -1,84 +1,78 @@
 <template>
-    <section class="groups">
-        <div v-for="group in getGroups" :key="group.id">
-            <group :group="group" />
+  <section class="groups">
+    <div v-for="group in getGroups" :key="group.id">
+      <group :group="group" />
+    </div>
+    <div class="add-list">
+      <label for="add" @click="toggleGroup" v-if="!isAddGroup">
+        <el-input
+          :style="{ width: '272px' }"
+          class="opacity-more"
+          name="add"
+          placeholder="+ Add a list"
+          v-model="newGroup.title"
+        ></el-input>
+      </label>
+        <div class="add-list-container" v-if="isAddGroup">
+          <label>
+            <el-input
+              placeholder="Enter list title..."
+              v-model="newGroup.title"
+              @keyup.enter.native="addGroup"
+            ></el-input>
+            <div class="btn-add">
+              <el-button type="primary" @click="addGroup"> Add list</el-button>
+              <span class="material-icons-outlined grey" @click="toggleGroup"
+                >close</span
+              >
+            </div>
+          </label>
         </div>
-        <div class="add-list">
-            <label for="add" @click="toggleGroup" v-if="!isAddGroup">
-                <el-input
-                :style="{width:'272px'}"
-                class="opacity-more "
-                    name="add"
-                    placeholder="+ Add a list"
-                    v-model="newGroup.title"
-                ></el-input>
-            </label>
-             <transition name="fade">
-<div class="add-list-container" v-if="isAddGroup">
-
-            <label  >
-                <el-input
-                    placeholder="Enter list title..."
-                    v-model="newGroup.title"
-                     @keyup.enter.native="addGroup"
-                ></el-input>
-<div class="btn-add"> 
-
-
-                <el-button type="primary" @click="addGroup">
-                    Add list</el-button
-                >
-                <span class="material-icons-outlined grey" @click="toggleGroup">close</span>
-</div>
-                <!-- <i class="el-icon-close" @click="toggleGroup"></i> -->
-            </label>
-</div>
-</transition>
-        </div>
-    </section>
+    </div>
+  </section>
 </template>
 
 <script>
-import { boardService } from '../service/board.service';
-import group from '../cmp/group.vue';
+import { boardService } from "../service/board.service";
+import group from "../cmp/group.vue";
 
 export default {
-    name: 'groups',
-    components: {
-        group,
+  name: "groups",
+  components: {
+    group,
+  },
+  data() {
+    return {
+      isAddGroup: false,
+      newGroup: boardService.getEmptyGroup(),
+    };
+  },
+  created() {},
+  methods: {
+    toggleGroup() {
+      this.isAddGroup = !this.isAddGroup;
     },
-    data() {
-        return {
-            isAddGroup: false,
-            newGroup: boardService.getEmptyGroup(),
-        };
-    },
-    created() {},
-    methods: {
-        toggleGroup() {
-            this.isAddGroup = !this.isAddGroup;
-        },
 
-        async addGroup() {
-            try {
-                console.log(this.newGroup);
-                var res = await this.$store.dispatch({
-                    type: 'addGroup',
-                    newGroup: this.newGroup,
-                });
-                this.newGroup = boardService.getEmptyGroup();
-                console.log(res);
-            } catch (err) {
-                console.log(err);
-            }
-        },
+    async addGroup() {
+      try {
+        console.log(this.newGroup);
+        var res = await this.$store.dispatch({
+          type: "addGroup",
+          newGroup: this.newGroup,
+        });
+        this.newGroup = boardService.getEmptyGroup();
+        console.log(res);
+      } catch (err) {
+        console.log(err);
+      }
     },
-    computed: {
-        getGroups() {
-            console.log(this.$store.getters.getBoard);
-            return this.$store.getters.getBoard.groups;
-        },
+  },
+  computed: {
+    getGroups() {
+      console.log(this.$store.getters.getBoard);
+      return this.$store.getters.getBoard.groups;
     },
+  },
 };
 </script>
 
