911ca5c74cc77a9707fceb14d1825be7aee3c5c2
Aviel Cohanian <avial151515@gmail.com>
2021-12-06 14:51:18 +0200

    att

--- src/service/img.service.js
+++ src/service/img.service.js
@@ -1,45 +1,41 @@
 import axios from 'axios';
 
 export const uploadImg = (ev) => {
-    const UPLOAD_PRESET = 'trelix-casep2021'; //insert yours
-    const CLOUD_NAME = 'trelix-casep21'; //insert yours
-    const UPLOAD_URL = `https://api.cloudinary.com/v1_1/${CLOUD_NAME}/image/upload`;
-    const FORM_DATA = new FormData();
-    FORM_DATA.append('file', ev.target.files[0]);
-    FORM_DATA.append('upload_preset', UPLOAD_PRESET);
-    return fetch(UPLOAD_URL, {
-        method: 'POST',
-        body: FORM_DATA,
-    })
-        .then((res) => res.json())
-        .then((res) => res)
-        .catch((err) => console.error(err));
+  const UPLOAD_PRESET = 'trelix-casep2021'; //insert yours
+  const CLOUD_NAME = 'trelix-casep21'; //insert yours
+  const UPLOAD_URL = `https://api.cloudinary.com/v1_1/${CLOUD_NAME}/image/upload`;
+  const FORM_DATA = new FormData();
+  FORM_DATA.append('file', ev.target.files[0]);
+  FORM_DATA.append('upload_preset', UPLOAD_PRESET);
+  return fetch(UPLOAD_URL, {
+    method: 'POST',
+    body: FORM_DATA,
+  })
+    .then((res) => res.json())
+    .then((res) => res)
+    .catch((err) => console.error(err));
 };
 export const imgService = {
-    getImgs,
+  getImgs,
 };
-<<<<<<< HEAD
-async function getImgs(searchBy) {
-    const url = `https://api.unsplash.com/search/photos?page=1&per_page=12&query=${searchBy}&orientation=landscape&client_id=Zgu8W39Fmue2y2khdV4U2EIAqj__wKs8Fk6oL6xn_bU`;
-=======
-async function getImgs(searchBy,size=12) {
-    const url = `https://api.unsplash.com/search/photos?page=1&per_page=${size}
+
+async function getImgs(searchBy, size = 12) {
+  const url = `https://api.unsplash.com/search/photos?page=1&per_page=${size}
     &query=${searchBy}&orientation=landscape&client_id=Zgu8W39Fmue2y2khdV4U2EIAqj__wKs8Fk6oL6xn_bU`;
->>>>>>> 3ab23e50225f31694e5b6ea6e54b437cc2d6702e
-    try {
-        const imgs = await axios.get(url);
-        return Promise.resolve(filterImgs(imgs.data));
-    } catch (err) {
-        throw err;
-    }
+  try {
+    const imgs = await axios.get(url);
+    return Promise.resolve(filterImgs(imgs.data));
+  } catch (err) {
+    throw err;
+  }
 }
 function filterImgs(imgs) {
-    const newImgs = imgs.results.map((item) => {
-        return {
-            full: { backgroundImage: `url(${item.urls.full})` },
-            small: { backgroundImage: `url(${item.urls.small})` },
-            description: item.description,
-        };
-    });
-    return newImgs;
+  const newImgs = imgs.results.map((item) => {
+    return {
+      full: { backgroundImage: `url(${item.urls.full})` },
+      small: { backgroundImage: `url(${item.urls.small})` },
+      description: item.description,
+    };
+  });
+  return newImgs;
 }
