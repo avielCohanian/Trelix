3d3f4284a12c4e2ae956ada32aa75c003321570b
Aviel Cohanian <avial151515@gmail.com>
2021-12-06 02:29:38 +0200

    add backend

--- src/store/user.store.js
+++ src/store/user.store.js
@@ -4,47 +4,45 @@ import { userService } from '../service/user-service.js';
 
 export const userStore = {
     state: {
-        userLog : null
+        userLog: null,
     },
     getters: {
-        getUserConnect(state){
-            console.log(state.userLog);
-            return state.userLog
+        getUserConnect(state) {
+            return state.userLog;
         },
-        
     },
     mutations: {
-        logIn(state,{userLog}){
-            state.userLog = userLog
-            console.log(state.userLog);
+        logIn(state, { userLog }) {
+            state.userLog = userLog;
         },
-        changeFavorit(state,{updateUser}){
-            state.userLog = updateUser
-        }
-        
+        changeFavorit(state, { updateUser }) {
+            state.userLog = updateUser;
+        },
     },
     actions: {
-        async logIn({commit},{userName}){
-            try{
-                const userLog =await userService.logIn(userName)
-                commit({type:'logIn',userLog})
-            }catch(err){
-                throw err
+        async logIn({ commit }, { userName }) {
+            try {
+                const userLog = await userService.logIn(userName);
+                commit({ type: 'logIn', userLog });
+            } catch (err) {
+                throw err;
             }
         },
-       async changeFavorit({commit,state,dispatch},{change}){
-           console.log(change);
-           try{
-              const copyDetails= JSON.parse(JSON.stringify({idBoard:change.idBoard,isFavorit:change.isFavorit,userLog: state.userLog}))
-               const updateUser = await userService.changeFevorit(copyDetails)
-               commit({type:'changeFavorit',updateUser})
-            //    this.userLog =userUp
-               console.log(updateUser);
-
-            }catch(err){
-                throw err
+        async changeFavorit({ commit, state, dispatch }, { change }) {
+            try {
+                const copyDetails = JSON.parse(
+                    JSON.stringify({
+                        idBoard: change.idBoard,
+                        isFavorit: change.isFavorit,
+                        userLog: state.userLog,
+                    })
+                );
+                const updateUser = await userService.changeFevorit(copyDetails);
+                commit({ type: 'changeFavorit', updateUser });
+                //    this.userLog =userUp
+            } catch (err) {
+                throw err;
             }
-       }
-
+        },
     },
 };
